26692105cd4025482a15e89fec449c9ded9300c7
Tushar Kataria <tushar15184@iiitd.ac.in>
2017-01-13 15:44:52 +0530

    Added precprocessing.

--- assignment1/withoutmeta.py
+++ assignment1/withoutmeta.py
@@ -1,4 +1,4 @@
-from sys import argv
+from sys import argv,exit
 
 #Opening meta-data file
 with open('metadata.csv') as meta_file:
@@ -15,5 +15,26 @@ with open('database.csv') as data_file:
 if(len(argv)==2):
 	print("Computing Sum:")
 	print(argv[1])
+	present = 0
+	for i in range(no_of_attr):
+		if(meta_data[i][0]==argv[1]):
+			field_name = meta_data[i][0]
+			field_data_type = meta_data[i][1]
+			field_size = meta_data[i][2]
+			present=1
+			break
+	if(present==0):
+		print "Attribute not present"
+		exit()
+
+	print "Attribute Found."
+	print field_name,field_data_type,field_size
+	#add more
+	if(field_data_type=='F' or field_data_type=='I'):
+		#add wala dal de
+		print "SUM"
+	else:
+		print "Character type argument!"
+
 else:
-	print("Invalid argument!")
+	print "Invalid argument!"
